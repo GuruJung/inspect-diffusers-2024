910dfaf4321d6f46a2d50c6606b5ddeaa16251db
samuel <getpgm1@gmail.com>
2024-09-10 14:20:57 +0900

    added test.py, test.ipynb.

---
+++ tools/test.py
@@ -0,0 +1,40 @@
+import os
+import torch
+
+from diffusers import StableVideoDiffusionPipeline
+from diffusers.utils import load_image, export_to_video
+
+def custom_repr(self):
+    return f'{{Tensor:{tuple(self.shape)}}} {original_repr(self)}'
+
+if __name__ == '__main__':
+    torch.set_printoptions(sci_mode=False)
+    original_repr = torch.Tensor.__repr__
+    torch.Tensor.__repr__ = custom_repr
+
+device = torch.device("mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu")
+
+pipe = StableVideoDiffusionPipeline.from_pretrained(
+    "stabilityai/stable-video-diffusion-img2vid-xt", torch_dtype=torch.float16, variant="fp16"
+).to(device)
+
+pipe.enable_model_cpu_offload()
+
+print(os.getcwd())
+
+# Load the conditioning image
+image = load_image("./images/fire-car-11-700.png")
+image = image.resize((1024, 576))
+image.resize((512, 288))    # convience for previewing
+
+
+generator = torch.manual_seed(42)
+frames = pipe(image, decode_chunk_size=8, generator=generator).frames[0]
+
+print(len(frames), frames)
+
+export_to_video(frames, "./results/fire-car-11-700.mp4", fps=7)
+
+
+
+
